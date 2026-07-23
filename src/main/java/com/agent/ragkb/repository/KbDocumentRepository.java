66c85f4c22dd754ecd230a5e33562ecf05200c14
EM-FF <2963513766@qq.com>
2026-07-23 16:58:14 +0800

    操作文档主表kb_document,保存文件信息，状态，版本

--- src/main/java/com/agent/ragkb/repository/KbDocumentRepository.java
+++ src/main/java/com/agent/ragkb/repository/KbDocumentRepository.java
@@ -7,6 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface KbDocumentRepository extends JpaRepository<KbDocument, Long> {
+    /**
+     * 操作文档主表
+     * kb_document
+     * 保存:
+     * 文件信息
+     * 状态
+     * 版本
+      */
 
     List<KbDocument> findByKbIdAndIsDeletedFalse(Long kbId);
 
