6fd7af2eb8ec4df428dd7f2f4f348293d32575e8
EM-FF <2963513766@qq.com>
2026-07-23 17:00:48 +0800

    操作索引任务index_task

--- src/main/java/com/agent/ragkb/repository/IndexTaskRepository.java
+++ src/main/java/com/agent/ragkb/repository/IndexTaskRepository.java
@@ -6,6 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface IndexTaskRepository extends JpaRepository<IndexTask, Long> {
+    /**
+     * 操作索引任务 index_task
+     * ，保存:
+     *  任务状态
+     *  失败次数
+     *  错误信息
+     */
 
     Optional<IndexTask> findTopByDocIdOrderByCreatedAtDesc(Long docId);
 }
